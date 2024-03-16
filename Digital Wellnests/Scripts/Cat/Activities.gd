56083d2657599f6018efa166213d802b8a201e39
givenmnisi6 <givenmnisi6@gmail.com>
2024-03-16 15:29:39 +0200

    Update Activities.gd

--- Digital Wellnests/Scripts/Cat/Activities.gd
+++ Digital Wellnests/Scripts/Cat/Activities.gd
@@ -1,21 +1,24 @@
 extends RigidBody2D
 
-var coin: bool
+var coin: bool  # Indicates whether the object is a coin or not
 
 func _ready():
+	# Play the ActivitiesAnim animation and select a random frame
 	$ActivitiesAnim.play()
 	var activities = $ActivitiesAnim.sprite_frames.get_animation_names()
 	$ActivitiesAnim.play(activities[randi() % activities.size()])
 
 func _process(delta):
-	pass
+	pass  # No processing needed in this function
 
 func coinHit():
+	# Play the sound effect for when the object is a coin
 	var audio = $Effects
 	audio.stream = load("res://Audio/Effects/aRight2.wav")
 	audio.play()
 
 func bullHit():
+	# Play the sound effect for when the object is not a coin
 	var audio = $Effects
 	audio.stream = load("res://Audio/Effects/aWrong.wav")
 	audio.play()
@@ -30,5 +33,5 @@ func _on_area_2d_area_entered(area):
 			coinHit()
 		else:
 			bullHit()
-		await audio.finished
-		queue_free()
+		await audio.finished  # Wait for the audio to finish playing
+		queue_free()  # Free the object from the scene
