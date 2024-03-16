09e6b269fbce2ec0fc8661e560142115732c9c24
givenmnisi6 <givenmnisi6@gmail.com>
2024-03-16 14:52:52 +0200

    Update Music.gd

--- Digital Wellnests/Scripts/Music.gd
+++ Digital Wellnests/Scripts/Music.gd
@@ -6,14 +6,15 @@ var intro = load("res://Audio/Effects/Jungle.wav")
 func _ready():
 	pass 
 
-# Play, stop and increase the Music functions
+# Play the music
 func playMusic():
 	$AudioStreamPlayer.stream = intro
 	$AudioStreamPlayer.play()
 
+# Stop the music
 func stopMusic():
 	$AudioStreamPlayer.volume_db = -40
 	
+# Increase the volume of the music
 func increaseVolume():
 	$AudioStreamPlayer.volume_db = 1
-
