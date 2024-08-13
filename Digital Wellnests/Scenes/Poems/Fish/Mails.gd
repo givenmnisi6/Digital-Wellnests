82c5f9d20646c20833b674c145d22122567fdc1d
givenmnisi6 <givenmnisi6@gmail.com>
2024-08-14 00:29:54 +0200

    Update Mails.gd

--- Digital Wellnests/Scenes/Poems/Fish/Mails.gd
+++ Digital Wellnests/Scenes/Poems/Fish/Mails.gd
@@ -15,15 +15,17 @@ func _on_button_pressed() -> void:
 	
 	if spamEmail == false:
 		print("Spam email detected")
-		audio.stream = load("res://Audio/Effects/aWrong.wav")
-		audio.play()
+		#audio.stream = load("res://Audio/Effects/aWrong.wav")
+		#audio.play()
+		Music.wrongSfx()
 	else:
-		audio.stream = load("res://Audio/Effects/aRight2.wav")
-		audio.play()
+		Music.rightSfx()
+		#audio.stream = load("res://Audio/Effects/aRight2.wav")
+		#audio.play()
 	
 	#_on_timer_timeout()
 	#audio.play()
-	await _on_effects_finished() # Wait for the audio to finish
+	#await _on_effects_finished() # Wait for the audio to finish
 	queue_free()
 	
 	#print("spamEmail is set to: ", self.spamEmail)
@@ -51,5 +53,5 @@ func _on_timer_timeout() -> void:
 	queue_free()
 
 
-func _on_effects_finished() -> void:
-	pass # Replace with function body.
+#func _on_effects_finished() -> void:
+	#pass # Replace with function body.
