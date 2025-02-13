980f661a7794cf4f49c47f47946c343ef3d214a1
givenmnisi6 <givenmnisi6@gmail.com>
2025-02-13 10:16:23 +0200

    Update Pause.gd

--- Digital Wellnests/Scripts/Basic/Pause.gd
+++ Digital Wellnests/Scripts/Basic/Pause.gd
@@ -17,6 +17,12 @@ func resumeGame():
 func pauseGame():
 	get_tree().paused = true
 
+func pauseAnimation():
+	%AnimationPlayer.play("PopOut")
+
+#func resumeAnimation():
+	#%AnimationPlayer.play_backwards("PopOut")
+
 # Quits the game
 func quitGame():
 	get_tree().quit()
@@ -40,4 +46,5 @@ func _on_exit_button_pressed() -> void:
 
 func _on_menu_button_pressed() -> void:
 	get_tree().paused = false
+	
 	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
