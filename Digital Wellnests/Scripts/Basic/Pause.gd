93d0da36344b7e6c375d0523a9ff38209f94d81b
givenmnisi6 <givenmnisi6@gmail.com>
2025-02-25 11:40:11 +0200

    Added Restart mechanism

--- Digital Wellnests/Scripts/Basic/Pause.gd
+++ Digital Wellnests/Scripts/Basic/Pause.gd
@@ -47,4 +47,32 @@ func _on_exit_button_pressed() -> void:
 func _on_menu_button_pressed() -> void:
 	get_tree().paused = false
 	
-	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
+	# Get the parent scene, the Main scene and return to it and free the game instance
+	var mainScene = game.get_parent()
+	mainScene.returnToMain()
+	game.queue_free()
+
+
+func _on_restart_button_pressed() -> void:
+	# Unpause the game
+	get_tree().paused = false
+	restartGame()
+
+func restartGame():
+	# Unpause the game
+	get_tree().paused = false
+	
+	# Get the parent (Main scene)
+	var main_scene = game.get_parent()
+	
+	# Store the current game index (make sure to use the correct property)
+	var current_game_index = game.gameIndex
+	
+	# Remove the old game instance
+	game.queue_free()
+	
+	# Set the story index on the main scene
+	main_scene.iStory = current_game_index
+	
+	# Start a new game with the same story
+	main_scene.startGame()
