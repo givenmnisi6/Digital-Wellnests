5decf318c288c9192ba7663e5ad50e815f6fd3ed
givenmnisi6 <givenmnisi6@gmail.com>
2025-02-27 21:15:17 +0200

    Create Settings.gd

---
+++ Digital Wellnests/Scripts/Basic/Settings.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var musicBus = AudioServer.get_bus_index("Music")
+
+func _ready():
+	%AnimationPlayer.play("PopOut")
+
+func _on_back_pressed():
+	# Play the click sound effect
+	Music.clickSfx()
+	
+	# Hide the SettingsPage node
+	#%AnimationPlayer.play_backwards("PopOut")
+	queue_free()
+	#$SettingsPage.hide()
+
+
+func _on_sound_button_pressed() -> void:
+	# Toggle the mute state of the music bus
+	AudioServer.set_bus_mute(musicBus, not AudioServer.is_bus_mute(musicBus))
+	Music.clickSfx()
