d488a7f7e77a302b243e39bf6408846f3bcbe5fa
givenmnisi6 <givenmnisi6@gmail.com>
2025-03-09 10:44:27 +0200

    Update Window.gd

--- Digital Wellnests/Scripts/Basic/Window.gd
+++ Digital Wellnests/Scripts/Basic/Window.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var opButton = $HBoxContainer/OptionButton
 
-# Creating a windows mode string array
+# These options will be displayed in the dropdown menu
 const WINDOW_MODE : Array[String] = [
 	"Windowed",
 	"Fullscreen",
@@ -11,7 +11,7 @@ const WINDOW_MODE : Array[String] = [
 ]
 
 func _ready():
-	# Select items in the dropdown menu
+	# Populate the dropdown with window mode options and connect the signal
 	addWindowItems()
 	opButton.item_selected.connect(_on_window_selected)
 
@@ -21,22 +21,19 @@ func addWindowItems() -> void:
 
 func _on_window_selected(index: int) -> void:
 	match index:
-		# Window Mode
+		# Window Mode - Standard window with borders
 		0: 
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
-		# Fullscreen mode
+		# Fullscreen mode - Covers the entire screen with borders
 		1: 
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
-		# Borderless Window
+		 # Borderless Window - Window without borders but not covering the entire screen
 		2:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
-		# Borderless Fullscreen
+		# Borderless Fullscreen - Covers the entire screen without borders
 		3: 
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
-
-# func _on_option_button_item_selected(index):
-#	pass # Replace with function body.
